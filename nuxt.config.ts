edabca750f53087102afb71a2f59726d0985c060
Zoro Raka <rakawidhiantoro@gmail.com>
2024-12-04 16:54:04 +0700

    added meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,5 +15,31 @@ export default defineNuxtConfig({
     },
     routes: ["/api/add", "/api/cron/scan", "/api/cron/audit", "/api/cron/detail"]
   },
+  app: {
+    head: {
+      charset: "UTF-8",
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Kritisi - Security Audit Explorer for Solidity',
+      meta: [
+        { name: 'description', content: 'AI-powered tool for analyzing Solidity contracts on Arbitrum. Detect vulnerabilities and get security scores with Gemini AI.' },
+        { name: 'keywords', content: 'smart contract, security audit, solidity, blockchain, arbitrum, AI, security analysis, Gemini AI, security scoring' },
+        { name: 'author', content: 'Raka' },
+        { name: 'robots', content: 'index, follow' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://explorer.kritisi.xyz' },
+        { property: 'og:title', content: 'Kritisi - Security Audit Explorer for Solidity' },
+        { property: 'og:description', content: 'AI-powered smart contract security analysis tool for Arbitrum Chain. Detect vulnerabilities and get comprehensive security scores for your Solidity contracts. Powered by Gemini AI for auditing and scoring.' },
+        { property: 'og:image', content: 'https://www.kritisi.xyz/card-image.png' },
+        { property: 'twitter:card', content: 'summary_large_image' },
+        { property: 'twitter:url', content: 'https://explorer.kritisi.xyz' },
+        { property: 'twitter:title', content: 'Kritisi - Security Audit Explorer for Solidity' },
+        { property: 'twitter:description', content: 'AI-powered smart contract security analysis tool for Arbitrum Chain. Detect vulnerabilities and get comprehensive security scores for your Solidity contracts. Powered by Gemini AI for auditing and scoring.' },
+        { property: 'twitter:image', content: 'https://www.kritisi.xyz/card-image.png' },
+      ],
+      script: [
+        { src: 'https://www.googletagmanager.com/gtag/js?id=G-4JVF7SBVV2', async: true }
+      ]
+    },
+  },
   compatibilityDate: "2024-11-27"
 })
