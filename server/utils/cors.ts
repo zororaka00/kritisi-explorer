fecae66085f161a9454abfe8cc8de91dfe4d32b9
Zoro Raka <rakawidhiantoro@gmail.com>
2024-12-06 01:52:36 +0700

    delete host

--- server/utils/cors.ts
+++ server/utils/cors.ts
@@ -2,7 +2,7 @@ export class CorsUtils {
     isAllowedOrigin(event: any): boolean {
         const config = useRuntimeConfig();
         const allowedOrigins = config.nodeEnv === 'development' ? 'localhost' : 'explorer.kritisi.xyz';
-        const origin = event.node.req.headers.origin || event.node.req.headers.host;
+        const origin = event.node.req.headers.origin;
         return origin?.includes(allowedOrigins);
     }
 }
